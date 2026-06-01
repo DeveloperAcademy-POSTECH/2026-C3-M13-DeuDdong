d64bf8e3af8f7b00c3ca1dac557b601822989ae2
dojmj <joahae@ewhain.net>
2026-06-01 17:29:40 +0900

    Chore: UserInterfaceState 추적 제외 및 디버깅 UI 파일 추가

---
+++ ZupZup/ZupZup/Views/Components/ARDebugOverlayView.swift
@@ -0,0 +1,19 @@
+//
+//  ARDebugOverlayView.swift
+//  ZupZup
+//
+//  Created by 조민지 on 6/1/26.
+//
+
+import SwiftUI
+
+#if DEBUG
+struct ARDebugOverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARDebugOverlayView()
+}
