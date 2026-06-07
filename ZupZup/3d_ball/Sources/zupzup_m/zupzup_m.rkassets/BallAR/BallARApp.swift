dad993b5090719f9ec4c58adc66d3fc7fe309f01
yujinjeong1013 <yujjeong26@pos.idserve.net>
2026-06-07 14:54:16 +0900

    design: usda 구슬 추가

---
+++ ZupZup/3d_ball/Sources/zupzup_m/zupzup_m.rkassets/BallAR/BallARApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallARApp.swift
+//  BallAR
+//
+//  Created by YUJIN JEONG on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BallARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
